45ef1c768d9bbfe62db8d3913c9c11fec77de23f
b1zman <example@email.comtm121art@mail.ru>
2021-03-28 18:40:59 +0300

    added calculator, delete main

---
+++ homework_1/src/com/company/Calculator.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter first number " );
+        int num1 = in.nextInt();
+
+        System.out.print("Enter second number " );
+        int num2 = in.nextInt();
+
+        System.out.println("Введите операцию 1- сложение  2-вычитание 3-умножение 4-деление");
+        int operation = in.nextInt();
+        int result = 0;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+        }
+        if (operation >= 1 && operation <= 3) {
+            System.out.printf("Результат операции: %d", result);
+        }
+    }
+}
