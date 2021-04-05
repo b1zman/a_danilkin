81dc3f9b08c67cd46f622a3ec599b7ec12051386
b1zman <example@email.comtm121art@mail.ru>
2021-04-05 19:17:06 +0300

    formula G

---
+++ stringMss/src/com/company/StringOperation.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class TochkaG {
+    public static void main(String[] args) {
+    int a,p;
+    double m1,m2;
+
+    Scanner.in = new Scanner(System.in);
+    a=in.nextInt()
+    }
+}
+
+
+
+
+
